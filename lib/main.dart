38ca27cb5ceeea9c5210cdf5737af71203b2b1cb
hsyntes <se.hsyntes@gmail.com>
2024-08-06 15:49:13 +0300

    system ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:mobile/app.dart";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFFEE7E21),
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
   runApp(const MyApp());
 }
--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 class MyThemeData {
   static ThemeData get theme => ThemeData(
-          colorScheme: const ColorScheme.light(
-        primary: Color(0xFFEE7E21),
-      ));
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFFEE7E21),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFEE7E21),
+          foregroundColor: Colors.white,
+        ),
+      );
 }
